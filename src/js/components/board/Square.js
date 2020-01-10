8d8c4b269516283e4479370a0765e0f89c4c5810
redchush <redchush@gmail.com>
2020-01-10 18:29:50 +0300

    minor changes

--- src/js/components/board/Square.js
+++ src/js/components/board/Square.js
@@ -2,17 +2,17 @@
 
 import React from "react";
 import * as PropTypes from "prop-types";
+import * as R from "ramda";
+
+let addColor =  (props,styles) => (props.color ? { ...styles, color: props.color } : styles);
+let addWidth = (props, styles) => (props.cellSize ? {...styles,
+  width: `${props.cellSize}px`,
+  height: `${props.cellSize}px`} : props.cellSize);
+
+let buildStyles = (props, styles={})=> R.compose(addColor, addWidth);
 
 function Square(props) {
-  let styles = {};
-  if(props.color){
-    styles.color = props.color;
-  }
-  if(props.cellSize){
-    let dimension =  props.cellSize + "px";
-    styles.width = dimension;
-    styles.height = dimension;
-  }
+  let styles = buildStyles(props);
 
   return (
     <button className="square"
